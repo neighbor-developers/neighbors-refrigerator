6803fa6aba744c1c5fa73d6a9920ac26bfd8244c
itaebro <plus100kt@gmail.com>
2022-07-29 07:05:05 +0900

    feat: user sharedPreference 추가

---
+++ app/src/main/java/com/neighbor/neighborsrefrigerator/data/UserSharedPreference.kt
@@ -0,0 +1,43 @@
+package com.neighbor.neighborsrefrigerator.data
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class UserSharedPreference(context: Context) {
+    private val prefsFilename = "UserPrefs"
+    private val prefs: SharedPreferences = context.getSharedPreferences(prefsFilename, 0)
+
+    fun getUserPrefs(key: String): String? {
+        return prefs.getString(key, "")
+    }
+
+    fun getUserPrefs(): UserData {
+        return UserData(
+            id = prefs.getString("id", "")!!.toInt(),
+            fbID = prefs.getString("fbID", "")!!,
+            email = prefs.getString("email", "")!!,
+            nickname = prefs.getString("nickname", "")!!,
+            homeAddr = prefs.getString("homeAddr", "")!!,
+            reportPoint = prefs.getString("reportPoint", "")!!.toInt(),
+            latitude = prefs.getString("latitude", "")!!.toDouble(),
+            longitude = prefs.getString("longitude", "")!!.toDouble(),
+            createdAt = prefs.getString("createdAt", "")!!
+        )
+    }
+
+    fun setUserPrefs(key:String, value: String) {
+        return prefs.edit().putString(key, value).apply()
+    }
+
+    fun setUserPrefs(person: UserData) {
+        prefs.edit().putString("id", person.id.toString())
+        prefs.edit().putString("fbId", person.fbID)
+        prefs.edit().putString("email", person.email)
+        prefs.edit().putString("nickname", person.nickname)
+        prefs.edit().putString("homeAddr", person.homeAddr)
+        prefs.edit().putString("reportPoint", person.reportPoint.toString())
+        prefs.edit().putString("latitude", person.latitude.toString())
+        prefs.edit().putString("longitude", person.longitude.toString())
+        prefs.edit().putString("createdAt", person.createdAt)
+    }
+}
