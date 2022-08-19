9f9bb913aa847bc679dec9571e285aff2c4d64c7
hyejinxx <haejinjung1110@gmail.com>
2022-08-19 16:27:59 +0900

    feat: pagingSource

---
+++ app/src/main/java/com/neighbor/neighborsrefrigerator/network/MyPagingSource.kt
@@ -0,0 +1,47 @@
+package com.neighbor.neighborsrefrigerator.network
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.neighbor.neighborsrefrigerator.data.PostData
+import com.neighbor.neighborsrefrigerator.viewmodels.ReqPostData
+import java.io.IOException
+
+private const val STARTING_PAGE_INDEX = 1
+
+class MyPagingSource(
+    private val reqPostData: ReqPostData
+): PagingSource<Int, PostData>() {
+    private val dbAccessModule = DBAccessModule()
+
+    // 스크롤시 데이터를 비동기식으로 가져오기 위한 함수
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, PostData> {
+        // LoadParams: 로드할 키와 항목 수, LoadResult: 로드 작업의 결과
+        return try {
+            val position = params.key?: STARTING_PAGE_INDEX
+            val result = dbAccessModule.getPostOrderByTime(page = position,reqPostData = reqPostData)
+
+            // 로드에 성공한 경우
+            LoadResult.Page(
+                data = result,
+                prevKey = when(position){
+                    STARTING_PAGE_INDEX -> null
+                    else -> position -1
+                },
+                nextKey = position + 1
+            )
+            // PagingSource가 더 이상 결과의 무결성을 보장할 수 없으므로 무효화되어야 하는 경우
+            LoadResult.Invalid()
+        } catch (exception: IOException) {
+            return LoadResult.Error(exception)
+        }
+
+
+    }
+    override fun getRefreshKey(state: PagingState<Int, PostData>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            state.closestPageToPosition(anchorPosition)?.prevKey?.plus(1)
+                ?: state.closestPageToPosition(anchorPosition)?.nextKey?.minus(1)
+        }
+    }
+
+}
